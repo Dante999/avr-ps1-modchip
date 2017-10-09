37d8d253d5c10bec34ada05e2b891461bc62a0d0
dante999 <dante999@users.noreply.github.com>
2017-10-09 21:03:32 +0200

    changed commen-style

--- debug.c
+++ debug.c
@@ -9,53 +9,60 @@
 #include "main.h"
 
 
-/*
- * initialize the debug I/O pin
+/*******************************************************************************
+ * @brief	initialize the debug I/O pin
+ *
+ * simply sets the debug-pin as an output with an initial LOW-level
  * 
  * @param	none
  * 
  * @return	none
  *
-*/
+*******************************************************************************/
 void debug_init() {
-	REG_DDR |= (1<<PIN_DEBUG);
+	REG_DDR  |=  (1<<PIN_DEBUG);
+	REG_PORT &= ~(1<<PIN_DEBUG);
 }
 
 
-/*
- * sets the debug pin to HIGH
+/*******************************************************************************
+ * @brief	sets the debug pin to HIGH
  * 
  * @param	none
  *
  * @return	none
  *  
-*/
+*******************************************************************************/
 void debug_setPinHigh() {
 	REG_PORT |= (1<<PIN_DEBUG);
 }
 
 
-/* 
- * sets the debug pin to LOW
+/*******************************************************************************
+ * @brief	sets the debug pin to LOW
  * 
  * @param	none
  * 
  * @return	none
-*/
+ *
+*******************************************************************************/
 void debug_setPinLow() {
 	REG_PORT &= ~(1<<PIN_DEBUG);
 }
 
 
-/*
- * toggles the debug pin
+/*******************************************************************************
+ * @brief	toggles the debug pin
  * 
- * if the pin is currently HIGH, it changes to LOW and vice versa
+ * if the pin is currently HIGH, the function will change the debug-pin to a
+ * LOW-Level. if the pin is currently LOW, the function will change the
+ * debug-pin to a HIGH-level
  * 
  * @param	none
  * 
  * @return	none
-*/
+ *
+*******************************************************************************/
 void debug_togglePin() {
 	REG_PORT ^= (1<<PIN_DEBUG);
 }
