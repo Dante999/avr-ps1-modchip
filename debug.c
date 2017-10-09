913e3f1712f953e219180e386efa5188c4e07275
dante999 <dante999@users.noreply.github.com>
2017-10-09 20:39:33 +0200

    added comments

--- debug.c
+++ debug.c
@@ -9,19 +9,53 @@
 #include "main.h"
 
 
-
+/*
+ * initialize the debug I/O pin
+ * 
+ * @param	none
+ * 
+ * @return	none
+ *
+*/
 void debug_init() {
 	REG_DDR |= (1<<PIN_DEBUG);
 }
 
+
+/*
+ * sets the debug pin to HIGH
+ * 
+ * @param	none
+ *
+ * @return	none
+ *  
+*/
 void debug_setPinHigh() {
 	REG_PORT |= (1<<PIN_DEBUG);
 }
 
+
+/* 
+ * sets the debug pin to LOW
+ * 
+ * @param	none
+ * 
+ * @return	none
+*/
 void debug_setPinLow() {
 	REG_PORT &= ~(1<<PIN_DEBUG);
 }
 
+
+/*
+ * toggles the debug pin
+ * 
+ * if the pin is currently HIGH, it changes to LOW and vice versa
+ * 
+ * @param	none
+ * 
+ * @return	none
+*/
 void debug_togglePin() {
 	REG_PORT ^= (1<<PIN_DEBUG);
 }
--- debug.h
+++ debug.h
@@ -9,13 +9,9 @@
 #define DEBUG_H_
 
 void debug_init();
-
 void debug_setPinHigh();
-
 void debug_setPinLow();
 void debug_togglePin();
-
-
 
 
 #endif /* DEBUG_H_ */
